1f5d8eaca442f92e3eda8b533da18baab51cddbe
Parkhhungwook <wookiegochild@naver.com>
2024-03-22 16:06:18 +0900

    자 드가자2

--- app/src/main/java/com/example/project_last/Item.kt
+++ app/src/main/java/com/example/project_last/Item.kt
@@ -1,27 +1,27 @@
 package com.example.project_last
 
 data class Item(
-    var id: Int, // Primary key
-    var rest_name: String,
-    var isvisit: Int,
-    var isdelivery: Int,
-    var date: String,
-    var rest_star: Double,
-    var rest_comment: String,
-    var main_image_uri: String,
-    var address: String,
-    var url: String,
-    var isfavor: Int,
-    var menu_name: String,
-    var menu_star: Double,
-    var menu_image_uri: String,
-    var menu_comment: String,
-    var price: Int,
-    var hashtag: String,
-    var category1: String,
-    var category2: String,
-    var category3: String,
-    var category4: String
+    var id: Int = 0, // Primary key
+    var rest_name: String = "",
+    var isvisit: Int = 0,
+    var isdelivery: Int = 0,
+    var date: String = "",
+    var rest_star: Double = 0.0,
+    var rest_comment: String = "",
+    var main_image_uri: String ="",
+    var address: String = "",
+    var url: String = "",
+    var isfavor: Int = 0,
+    var menu_name: String = "",
+    var menu_star: Double = 0.0,
+    var menu_image_uri: String = "",
+    var menu_comment: String = "",
+    var price: Int = 0,
+    var hashtag: String = "",
+    var category1: String = "",
+    var category2: String = "",
+    var category3: String = "",
+    var category4: String = ""
 ) {
     var selected: Boolean = false
 }
