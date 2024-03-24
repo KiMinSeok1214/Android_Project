b78e4baaf16ade36a401f5ab099d4128eb539018
Parkhhungwook <wookiegochild@naver.com>
2024-03-24 14:50:02 +0900

    3/24 최초 수정본 드가자

--- app/src/main/java/com/example/project_last/AddDiaryActivity.kt
+++ app/src/main/java/com/example/project_last/AddDiaryActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.WindowManager
 import android.widget.Toast
 import com.example.project_last.databinding.ActivityAddDiaryBinding
+
 import java.util.Random
 
 
@@ -18,30 +19,12 @@ class AddDiaryActivity : BaseActivity() {
         // 저장 버튼을 클릭하면 view에 있는 text들을 저장하고 db에 넣는다.
         binding.ivSave.setOnClickListener {
             val item = Item(
-                0,
-                binding.etRestName.text.toString(),
-                0,
-                0,
-                Random().nextInt(5).toString(),
-                Random().nextInt(5).toDouble(),
-                binding.etRestComment.text.toString(),
-                "image_uri",
-                "address",
-                "url",
-                0,
-                "치킨",
-                2.5,
-                "menu_uri",
-                "soso",
-                15000,
-                "hasgtag",
-                "",
-                "",
-                "",
-                ""
-            )
-            Toast.makeText(this, "저장되었습니다.", Toast.LENGTH_SHORT).show()
+                rest_name = binding.etRestName.text.toString().trim(),
+                rest_comment = binding.etRestComment.text.toString().trim(),
+                date = binding.etDate.text.toString().trim()
+                )
             db.insertItem(item)
+            Toast.makeText(this, "저장되었습니다.", Toast.LENGTH_SHORT).show()
         }
     }
 }
